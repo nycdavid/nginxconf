f79cb483eef872bfaaf17f3ac3813692895a0daa
David Ko <david.ko@velvetreactor.com>
2018-04-16 07:07:31 -0400

    Add test/feature for routes file ingestion

--- nginxconf.go
+++ nginxconf.go
@@ -1,1 +1,21 @@
 package nginxconf
+
+import (
+	"encoding/json"
+	"log"
+	"strings"
+)
+
+type NginxConf struct {
+	Routes []map[string]string `json:"routes"`
+}
+
+func NewNginxConf(routes *strings.Reader) *NginxConf {
+	var conf NginxConf
+	dec := json.NewDecoder(routes)
+	err := dec.Decode(&conf)
+	if err != nil {
+		log.Print(err)
+	}
+	return &conf
+}
--- nginxconf_test.go
+++ nginxconf_test.go
@@ -1,1 +1,23 @@
 package nginxconf
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestIngestRoutesFile(t *testing.T) {
+	routesRdr := strings.NewReader(`{
+    "routes": [
+      { "location": "/google", "proxy_pass": "http://www.google.com" },
+      { "location": "/elasticsearch", "proxy_pass": "http://www.elastic.co" }
+    ]
+  }`)
+	nginxConf := NewNginxConf(routesRdr)
+
+	routesCt := len(nginxConf.Routes)
+	if routesCt != 2 {
+		errMsg := fmt.Sprintf("Expected %d routes, got %d", 2, routesCt)
+		t.Error(errMsg)
+	}
+}
